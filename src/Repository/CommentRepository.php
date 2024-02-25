e7644495f16fdcf58bd826af484efaaa5ad0907f
kevdokimov <79169605859@ya.ru>
2024-02-25 18:14:08 +0300

    Clearing unnecessary comments

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,9 +4,14 @@ namespace App\Repository;
 
 use App\Entity\Comment;
 use App\Entity\Conference;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Query\Parameter;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use Exception;
 
 /**
  * @extends ServiceEntityRepository<Comment>
@@ -18,12 +23,43 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
+    private const DAYS_BEFORE_REJECTED_REMOVAL = 7;
     public const PAGINATOR_PER_PAGE = 2;
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
     }
+
+    public function countOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->select('COUNT(c.id)')->getQuery()->getSingleScalarResult();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function deleteOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->delete()->getQuery()->execute();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.state = :state_rejected or c.state = :state_spam')
+            ->andWhere('c.createdAt < :date')
+            ->setParameters(new ArrayCollection([
+                new Parameter('state_rejected','rejected'),
+                new Parameter('state_spam','spam'),
+                new Parameter('date',new DateTimeImmutable(-self::DAYS_BEFORE_REJECTED_REMOVAL.' days')),
+            ]))
+        ;
+    }
+
 
     public function getCommentPaginator(Conference $conference, int $offset): Paginator
     {
